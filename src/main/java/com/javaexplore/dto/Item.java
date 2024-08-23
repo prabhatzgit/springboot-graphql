b1dfae66f0d7b9789a1f7ac97987bdb4d19cf12d
mahan <prabhat.nist4@gmail.com>
2024-08-24 00:55:46 +0530

    first commit

---
+++ src/main/java/com/javaexplore/dto/Item.java
@@ -0,0 +1,18 @@
+package com.javaexplore.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Item {
+
+    private String name;
+    private String category;
+    private Float price;
+    private Integer stock;
+}
